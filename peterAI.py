389aab93188a01673b073ea60eb0de55d4fa8160
Aaron Ashley-Gaddis <aaronashley466@gmail.com>
2025-12-12 13:43:15 -0700

    Create a chatbot using NLTK with web browser integration
    This script implements a chatbot using NLTK that responds to user inputs with predefined phrases. It also opens a YouTube video in the web browser.

---
+++ peterAI.py
@@ -0,0 +1,30 @@
+import nltk
+import random
+import string
+import webbrowser
+
+from nltk.chat.util import Chat, reflections
+
+#needs the NLTK data downloaded, only for the first time though
+nltk.download('punkt')
+
+
+# define pairs, 
+pairs = [
+    [r"hi|hello|hey", ["Heheheheheheheh," "Sweet!", "Oh Jeez," "Holy crap!"]],
+    [r"what is your name\??", ["I'm Peter Griffin. Hehehehehehehehehehehehehehehehehehehehehehehehehehe"]],
+    [r"how are you\??", ["Bird Is The Word.", "Shutup meg."]],
+    [r"(.*) your favorite (.*)?", ["I have an idea so smart that my head would explode if I even began to know what I'm talking about."]],
+    [r"bye|exit|quit", ["Thanks for giving me an exuse to not to talk to my daughter.", "Come on, let's go drink until we can't feel feelings anymore."]],
+    [r"(.*)", ["Tell me more.", "Why do you say that?", "Interesting... go on."]]
+    [r"why are you like this\??", ["Now I may be an idiot, but there's one thing I am not sir, and that sir, is an idiot"]]
+    [r"whats your view on AI\?? ["Gosh, It's Not Like The Internet To Go Crazy About Something Small And Stupid.", "Whoa, Whoa... Lois, This Is Not My Batman Glass."]]
+    
+]
+
+chatbot = Chat(pairs, reflections)
+print("Alright, time to go to the clam with the boys. You could type 'quit' to exit to leave this stupid program or whatever Brain said about AI. Can't remember, maybe if I had a few beers, I'll remember. Probably not, but here's Conway Twitty!!!")
+url = "https://youtu.be/d2aeRg_yMSE?si=97HuOO0Alq7abc6n"
+webbrowser.open(url)
+print(f"Opened the video in your web browser: {url}")
+chatbot.converse()
